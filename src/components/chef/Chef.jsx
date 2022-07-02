ce18ed0458500c49f0df0396b60a69b54d6527e0
PauliVr <paulette.vr.10@gmail.com>
2022-07-02 12:04:31 -0500

    Adds the chef dashboars

---
+++ src/components/chef/Chef.jsx
@@ -0,0 +1,10 @@
+import LogOut from '../botones/logOut/LogOut';
+
+export default function Chef() {
+  return (
+    <section>
+      <h1>Hola Chef</h1>
+      <LogOut></LogOut>
+    </section>
+  );
+}
