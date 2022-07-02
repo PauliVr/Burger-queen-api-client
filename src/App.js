1f24f564f4dd612482f0bda6af76162fec62d69b
PauliVr <paulette.vr.10@gmail.com>
2022-07-02 12:02:14 -0500

    makes changes in the app for router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+// import './App.css';
+// import { Routes, Route } from 'react-router-dom';
+// import LogIn from './components/login/Login.js';
+// import Registro from './components/registro/Registro.js';
+// import AdminHome from './components/adminHome/AdminHome.jsx';
+// import Menu from './components/menu/Menu';
+// import Platillo from './components/platillo/Platillo';
+// import Loader from './components/loader/Loader.jsx';
+// import Cart from './components/menu/Cart';
+// import GeneralHome from './components/GeneralHome';
+
+// function App({ user }) {
+//   return (
+//     <div className='App'>
+//       <Routes>
+//         <Route path='/' element={<LogIn />} />
+//         <Route path='/general-home' element={<GeneralHome  />} />
+//         <Route path='/home' element={<AdminHome />}></Route>
+//         <Route path='/platillos' element={<Platillo />}></Route>
+//         <Route path='/registro' element={<Registro />} />
+//         <Route path='/informacion-pedidos' element={<AdminHome />}></Route>
+//         <Route path='/menu' element={<Menu />}></Route>
+//         <Route path='/pruebas' element={<Cart />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import LogIn from './components/login/Login.js';
-import Registro from './components/registro/Registro.js';
-import AdminHome from './components/adminHome/AdminHome.jsx';
-import Menu from './components/menu/Menu';
-import Platillo from './components/platillo/Platillo';
-import Loader from './components/loader/Loader.jsx';
+import React, { useState } from 'react';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import GlobalRouter from './router/GlobalRoutes';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(null);
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsAuth(user);
+    } else {
+      setIsAuth(null);
+    }
+  });
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<LogIn />} />
-        <Route path='/home' element={<AdminHome />}></Route>
-        <Route path='/platillos' element={<Platillo />}></Route>
-        <Route path='/registro' element={<Registro />} />
-        <Route path='/informacion-pedidos' element={<AdminHome />}></Route>
-        <Route path='/menu' element={<Menu />}></Route>
-      </Routes>
-    </div>
+    <section>
+      <GlobalRouter isAuth={isAuth} />
+    </section>
   );
 }
 
 export default App;
+//
